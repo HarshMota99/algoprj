6f684f97bb845edbda97974eaed606baf0809d9c
Harsh Mota <harsh.mothatechkb@gmail.com>
2019-06-26 07:37:34 -0700

    fadsf

---
+++ src/com/pratice/array/singleArray/TargetIndices.java
@@ -0,0 +1,27 @@
+package com.pratice.array.singleArray;
+
+public class TargetIndices {
+//	public static void main(String[] args) {
+//		int num[]= {2, 5, 9,10, 11, 15};
+//		int target = 15,
+//	TargetIndices res = new TargetIndices();
+//		System.out.println(Array,s.toString(res.sum(nums, target)));
+//
+//	}
+//	public int[] sum(int[] nums, int target) {
+//
+//
+//
+//	Map<Integer,Integer> map=new HashMap<Integer,Integer>();
+//	for(int i=0;i<num;i++) {
+//		int secondNum=target-nums[i];
+//		if(map.containsKey(secondNum)) {
+//			int [] res= {i,map.get(secondNum));
+//			return res;
+//			}
+//			map.put(nums[i], i);
+//			}
+//		return null
+//	}
+//	}
+}
